6664902fbceeac91fb4fce5215dcfed7b9d0b608
selkniez <samy.elkniez@sksystems.fr>
2026-07-10 21:09:35 +0200

    Refactor website structure: add header, footer, and new pages for About, News, Courses, Prices, and Contact; update navigation links and improve layout

--- src/Controller/MembershipPlanController.php
+++ src/Controller/MembershipPlanController.php
@@ -13,13 +13,47 @@ class MembershipPlanController extends AbstractController
 
     public function __construct(private readonly MembershipPlanRepository $membershipPlanRepository)
     {}
-   #[Route('/', name: 'app_membership-plan_index', methods: ['GET'])]
+    #[Route('/', name: 'app_membership-plan_index', methods: ['GET'])]
     public function index(): Response
+    {
+        return $this->render('membership_plan/index.html.twig');
+    }
+
+    #[Route('/a-propos', name: 'app_about', methods: ['GET'])]
+    public function about(): Response
+    {
+        return $this->render('membership_plan/about.html.twig');
+    }
+
+    #[Route('/actualites', name: 'app_news', methods: ['GET'])]
+    public function news(): Response
+    {
+        return $this->render('membership_plan/news.html.twig');
+    }
+
+    #[Route('/cours', name: 'app_courses', methods: ['GET'])]
+    public function courses(): Response
     {
         $membershipPlans = $this->membershipPlanRepository->findAll();
 
-        return $this->render('membership_plan/index.html.twig', [
+        return $this->render('membership_plan/courses.html.twig', [
             'membershipPlans' => $membershipPlans,
         ]);
+    }
+
+    #[Route('/tarifs', name: 'app_prices', methods: ['GET'])]
+    public function prices(): Response
+    {
+        $membershipPlans = $this->membershipPlanRepository->findAll();
+
+        return $this->render('membership_plan/prices.html.twig', [
+            'membershipPlans' => $membershipPlans,
+        ]);
+    }
+
+    #[Route('/contact', name: 'app_contact', methods: ['GET'])]
+    public function contact(): Response
+    {
+        return $this->render('membership_plan/contact.html.twig');
     }
 }
